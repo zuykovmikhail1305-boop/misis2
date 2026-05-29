e64409f63aa9c20006c81182d9e5afc4f0bc8482
Fr1ze <Mizu1305@yandex.ru>
2026-05-29 20:11:59 +0300

    a

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 from app.schemas import CreatePlanRequest, UpdatePlanRequest, TaskProgressRequest
 from app.fake_llm import generate_fake_learning_plan
@@ -25,14 +23,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Mount static files and templates
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/")
-def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+def root():
+    return {"message": "Backend is running"}
 
 
 @app.post("/plans")
